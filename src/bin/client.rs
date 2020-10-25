438b06aa24f7c2a3a52b45e403b778aae0f3d264
Arthur Coelho <arthurcoelho@pm.me>
2020-10-25 18:33:34 -0300

    hello client and server

---
+++ src/bin/client.rs
@@ -0,0 +1,42 @@
+use tungstenite::{connect, Message};
+use url::Url;
+
+extern crate env_logger;
+extern crate log;
+
+use log::info;
+
+use std::{thread, time};
+
+fn main() {
+    env_logger::init();
+    info!("Client started");
+
+    let sleep_time = time::Duration::from_secs(1);
+
+    let mut socket;
+    loop {
+        let connection = connect(Url::parse("ws://192.168.0.103:9001").unwrap());
+        match connection {
+            Ok(result) => {
+                info!("Connected");
+                socket = result.0;
+                break;
+            }
+            Err(error) => {
+                info!("Couldn't connect to server: {}", error);
+                thread::sleep(sleep_time);
+                info!("Retrying...");
+            }
+        }
+    }
+
+    let msg = "Hello server";
+    info!("Sending message {}", msg);
+    socket.write_message(Message::Text(msg.into())).unwrap();
+    loop {
+        let msg = socket.read_message().expect("Error reading message");
+        info!("Received: {}", msg);
+    }
+    //socket.close(None);
+}
